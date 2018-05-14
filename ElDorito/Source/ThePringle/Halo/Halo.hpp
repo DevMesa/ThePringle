2485b7dd449a538722cb51735fadbb066c6b2114
Ashleigh Adams <ashleigh.k.adams@gmail.com>
2018-05-14 21:28:26 +0100

    Added SurfaceNormal to TraceResult
    Stylecop strikes again too!

--- ElDorito/Source/ThePringle/Halo/Halo.hpp
+++ ElDorito/Source/ThePringle/Halo/Halo.hpp
@@ -7,17 +7,15 @@
 
 namespace Halo
 {
-	class trace_result_t
+	struct TraceResult
 	{
-	public:
 		Pringle::Vector N00000013; //0x0000
 		char pad_000C[8]; //0x000C
 		Pringle::Vector N00000016; //0x0014
 		int16_t hit2; //0x0020
 		int16_t hit3; //0x0022
-		int32_t N0000005A; //0x0024
-		int32_t N00000037; //0x0028
-		char pad_002C[8]; //0x002C
+		Pringle::Vector SurfaceNormal;
+		char pad_0030[4]; //0x0030
 		int16_t hit4; //0x0034
 		int32_t N00000046; //0x0036
 		char pad_003A[6]; //0x003A
@@ -38,9 +36,23 @@ namespace Halo
 		char pad_0091[43]; //0x0091
 	}; //Size: 0x00BC
 
+	inline bool Trace(TraceResult& result, Pringle::Vector start, Pringle::Vector end, uint32_t unitIndex1, uint32_t unitIndex2)
+	{
+		end = result.N00000013 = end - start;
+
+		uint32_t flags1 = (*(uint32_t*)0x471A8F8) | 0x200;
+		uint32_t flags2 = *(uint32_t*)0x471A8FC;
+
+		static const uint32_t addr = 0x6D7160;
+		auto fn = reinterpret_cast<bool(*)(uint32_t, uint32_t, Pringle::Vector*, Pringle::Vector*, uint32_t, uint32_t, TraceResult*)> (addr);
+		bool fn_result = fn(flags1, flags2, &start, &end, unitIndex1, unitIndex2, &result);
+
+		return !fn_result;
+	}
+
 	inline bool SimpleHitTest(Pringle::Vector start, Pringle::Vector end, uint32_t unitIndex1, uint32_t unitIndex2)
 	{
-		trace_result_t result;
+		TraceResult result;
 
 		result.N00000013 = end - start;
 		end = result.N00000013;
@@ -49,14 +61,15 @@ namespace Halo
 		uint32_t flags2 = *(uint32_t*)0x471A8FC;
 
 		static const uint32_t addr = 0x6D7160;
-		auto fn = reinterpret_cast<bool(*)(uint32_t, uint32_t, Pringle::Vector*, Pringle::Vector*, uint32_t, uint32_t, trace_result_t*)> (addr);
+		auto fn = reinterpret_cast<bool(*)(uint32_t, uint32_t, Pringle::Vector*, Pringle::Vector*, uint32_t, uint32_t, TraceResult*)> (addr);
 		bool fn_result = fn(flags1, flags2, &start, &end, unitIndex1, unitIndex2, &result);
 
 		return !fn_result;
 	}
+
 	/*inline bool SimpleHitTest(Pringle::Vector* start, Pringle::Vector* end, uint32_t unitIndex1, uint32_t unitIndex2)
 	{
-		trace_result_t result;
+		TraceResult result;
 
 		result.N00000013 = *end - *start;
 		*end = result.N00000013;
@@ -65,7 +78,7 @@ namespace Halo
 		uint32_t flags2 = *(uint32_t*)0x471A8FC;
 
 		static const uint32_t addr = 0x6D7160;
-		auto fn = reinterpret_cast<bool(*)(uint32_t, uint32_t, Pringle::Vector*, Pringle::Vector*, uint32_t, uint32_t, trace_result_t*) > (addr);
+		auto fn = reinterpret_cast<bool(*)(uint32_t, uint32_t, Pringle::Vector*, Pringle::Vector*, uint32_t, uint32_t, TraceResult*) > (addr);
 		bool fn_result = fn(flags1, flags2, start, end, unitIndex1, unitIndex2, &result);
 
 		return !fn_result;
